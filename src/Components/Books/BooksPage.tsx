f0e9a9a295a3dd2c303b21d22e09c97670ee134e
Chelsea Harper <chelsearharper@gmail.com>
2025-05-13 15:53:40 -0600

    updating books page

--- src/Components/Books/BooksPage.tsx
+++ src/Components/Books/BooksPage.tsx
@@ -1,8 +1,28 @@
+import './BooksPage.css';
+
 function BooksPage() {
+    console.log("book page reached");
     return (
-        <div>
-            This will be the books page.
-        </div>
+        <>
+            <div id="book1" className='book-container'>
+                <div className='bookimage'>
+                    <img src="/Images/wakeofthephoenix_ebook.png" alt="Cover for Wake of the Phoenix" />
+                </div>
+                <div className='booktext'>
+                    <h2>Wake of the Phoenix</h2>
+                    <p>Book description...</p>
+                </div>
+            </div>
+            <div id="book2" className='book-container'>
+                <div className='bookimage'>
+                    <img src="/Images/Tales_of_the_Laisian_Empire_eBook.png" alt="Cover for Wake of the Phoenix" />
+                </div>
+                <div className='booktext'>
+                    <h2>Tales of the Laisian Empire</h2>
+                    <p>Book description...</p>
+                </div>
+            </div>
+</>
     )
 }
 
