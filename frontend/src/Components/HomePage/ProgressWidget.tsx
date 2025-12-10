05a51a8c6f6b1f1d38f67fa14e3d8c2b13d79784
Chelsea Harper <seashell.smith@gmail.com>
2025-12-09 17:24:10 -0700

    Updating design for progress widget

--- frontend/src/Components/HomePage/ProgressWidget.tsx
+++ frontend/src/Components/HomePage/ProgressWidget.tsx
@@ -12,34 +12,35 @@ interface ProgressWidgetProps {
 function ProgressWidget({ projectTitle, drafting, selfEdit, proEdit, releasePrep }: ProgressWidgetProps) {
   return (
     <div className="progress-widget">
-        <h3 className="progress-title">{projectTitle}</h3>
+      <h3 className="progress-title">{projectTitle}</h3>
+
       <div className="circle-stack">
 
         <div
           className="progress-ring ring1"
-          style={{ 
-            background: `conic-gradient(#b56576 ${drafting}%, #222 ${drafting}%)`
+          style={{
+            background: `conic-gradient(var(--ring1) ${drafting}%, #222 ${drafting}%)`
           }}
         ></div>
 
         <div
           className="progress-ring ring2"
-          style={{ 
-            background: `conic-gradient(#6d597a ${selfEdit}%, #222 ${selfEdit}%)`
+          style={{
+            background: `conic-gradient(var(--ring2) ${selfEdit}%, #222 ${selfEdit}%)`
           }}
         ></div>
 
         <div
           className="progress-ring ring3"
-          style={{ 
-            background: `conic-gradient(#355070 ${proEdit}%, #222 ${proEdit}%)`
+          style={{
+            background: `conic-gradient(var(--ring3) ${proEdit}%, #222 ${proEdit}%)`
           }}
         ></div>
 
         <div
           className="progress-ring ring4"
-          style={{ 
-            background: `conic-gradient(#e56b6f ${releasePrep}%, #222 ${releasePrep}%)`
+          style={{
+            background: `conic-gradient(var(--ring4) ${releasePrep}%, #222 ${releasePrep}%)`
           }}
         ></div>
 
