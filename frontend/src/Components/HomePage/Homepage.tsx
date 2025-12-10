8b34ec0d39c10365e5f0830b6b880d4020fd087a
Chelsea Harper <seashell.smith@gmail.com>
2025-12-10 07:07:17 -0700

    minor final updates to content

--- frontend/src/Components/HomePage/Homepage.tsx
+++ frontend/src/Components/HomePage/Homepage.tsx
@@ -47,7 +47,26 @@ function Homepage({books} : {books: {id: string, title: string, coverImage: stri
                 <div className="release-text">
                     <p>A new, extended version of the first book in the Artifice of Power Saga,
                         <em>Wake of the Phoenix</em>, is set to be released on March 2, 2026.
+                        This new edition features:
                     </p>
+                    <ul>
+                        <li>New, Updated Cover!</li>
+                        <li>Additional Chapters!</li>
+                        <li>A brand new epilogue!</li>
+                        <li>A teaser of Book 2 after the main content!</li>
+                    </ul>
+                    <p>
+                        The rebellion is over, but for the war hero prince of Sentar Province,
+                        the troubles are just beginning. Caught between his duty to his people
+                        and his duty to the omnicient prophet he met--and learned to love--while
+                        fighting for justice against his corrupt leaders, he's the most powerful pawn
+                        the Laisian empire has ever seen. The thief whose very presence derails his
+                        lover's plans wants nothing to do with wither of them. But when did the poor
+                        ever get what<em> they </em> want? Together, they might find a way to heal
+                        the damage done by a brutal tyrant already dead. Or they might tear this
+                        province apart.
+                    </p>
+                    <p><strong>Preorder your copy of the extended edition today.</strong></p>
                 </div>
             </div>
           </div>
@@ -61,7 +80,7 @@ function Homepage({books} : {books: {id: string, title: string, coverImage: stri
             <h2>News & Updates</h2>
             <p>
                 The Kickstarter for the limited edition of <em>Wake of the Phoenix</em>, a hardback with
-                foil embossed dust jacket and featuring the last release of the original cover,
+                foil embossed dust jacket featuring the last release of the original cover,
                 is nearing final fulfillment! This project was funded back in mid-April and e-books
                 have already been sent to backers. The physical proof was received in late November
                 and approved early December, with orders already placed for final fulfillment. If you
